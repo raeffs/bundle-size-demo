61df419e9989adfb4f4d07778038dd418018b6f8
Raphael Fleischlin <git@raefftec.ch>
2020-09-13 13:37:20 +0200

    better imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+
+/*
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import * as lodash from 'lodash';
+*/
+
+import { interval } from 'rxjs';
+import get from 'lodash/get';
+import set from 'lodash/set';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +20,15 @@ export class AppComponent {
   public counter = 0;
 
   constructor() {
+    /*
     Observable.interval(1000).subscribe(() => {
       const current = lodash.get(this, 'counter');
       lodash.set(this, 'counter', current + 1);
+    });
+    */
+    interval(1000).subscribe(() => {
+      const current = get(this, 'counter');
+      set(this, 'counter', current + 1);
     });
   }
 }
