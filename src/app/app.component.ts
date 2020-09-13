e881455173586134d63bca938493921080024ef9
Raphael Fleischlin <git@raefftec.ch>
2020-09-13 11:56:55 +0200

    create demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bundle-size-demo';
+  public title = 'bundle-size-demo';
+
+  public counter = 0;
+
+  constructor() {
+    Observable.interval(1000).subscribe(() => {
+      const current = lodash.get(this, 'counter');
+      lodash.set(this, 'counter', current + 1);
+    });
+  }
 }
